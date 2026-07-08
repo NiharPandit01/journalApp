4caa5f4a0cfb6c532e7f9a5c76b44d6449605d80
Nihar <panditnihar919@gmail.com>
2026-07-08 10:23:42 +0530

    added the bean annonation omn mycustom in swaggerconfig

--- src/main/java/net/engineeringdigest/journalApp/config/SwaggerConfig.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SwaggerConfig.java
@@ -2,10 +2,12 @@ package net.engineeringdigest.journalApp.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    @Bean
     public OpenAPI mycustom(){
         return  new OpenAPI().info(new Info().title("journal App APIs").description("by Nihar"));
     }
